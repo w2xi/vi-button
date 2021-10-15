6410fc3dc0574da949fe1e98cbcdb3c31f3cb25e
wangjigang <wangjigang@xingshentech.com>
2021-10-15 18:45:21 +0800

    A default style button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vi-button>Button</vi-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ViButton from '@/components/button/index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ViButton
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="stylus" scoped>
+
+#app
+  text-align center
+  margin-top 50px
+
+</style>>
---
+++ src/components/button/index.vue
@@ -0,0 +1,77 @@
+<template>
+	<button
+		:class="['vi-button', this.className]"
+	>
+		<slot />
+	</button>
+</template>
+
+<script>
+
+const typeList = [
+	'primary', 'info', 'warning', 'danger'
+]
+
+export default {
+	props: {
+		type: {
+			type: String,
+			default: '',
+		},
+
+		size: {
+			type: String,
+			default: 'large',
+		},
+
+		icon: {
+			type: String,
+		},
+	},
+
+	data(){
+		return {
+			className: 'primary'
+		}
+	},
+
+	created(){
+		if ( typeList.indexOf(this.type) !== -1  ){
+			this.className = this.type
+		}
+	},
+
+	methods: {
+		
+	},
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.vi-button
+	display: inline-block
+	line-height: 1
+	white-space: nowrap
+	appearance: none
+	font-size: 14px
+	font-weight: 500
+	text-align: center
+	outline: none
+	color: #606266
+	background-color: #fff
+	border: 1px solid #dcdfe6
+	margin: 0
+	padding: 12px 20px	
+	border-radius: 4px
+	box-sizing: border-box
+	cursor: pointer
+	transition: 0.1s
+
+.vi-button:focus,
+.vi-button:hover
+	color: #409eff
+	border-color: #c6e2ff
+	background-color: #ecf5ff
+
+</style>
