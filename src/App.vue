493022a9db4f7fac7b597f8a4a7b5a346124a5d1
w2xi <43wangxi@gmail.com>
2021-10-15 23:37:53 +0800

    style button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <vi-button>Button</vi-button>
+    <div class="row">
+      <vi-button>Default</vi-button>
+      <vi-button type="primary">Primary</vi-button>
+      <vi-button type="success">Success</vi-button>
+      <vi-button type="info">Info</vi-button>
+      <vi-button type="warning">Warning</vi-button>
+      <vi-button type="danger">Danger</vi-button>
+    </div>
+
+    <div class="row">
+      <vi-button plain>Plain Default</vi-button>
+      <vi-button type="primary" plain>Plain Primary</vi-button>
+      <vi-button type="success" plain>Plain Success</vi-button>
+      <vi-button type="info" plain>Plain Info</vi-button>
+      <vi-button type="warning" plain>Plain Warning</vi-button>
+      <vi-button type="danger" plain>Plain Danger</vi-button>
+    </div>
   </div>
 </template>
 
@@ -22,4 +38,11 @@ export default {
   text-align center
   margin-top 50px
 
+.row
+  margin-top: 15px
+
+.vi-button + .vi-button
+  margin-left: 10px
+
 </style>>
+
--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -9,8 +9,10 @@
 <script>
 
 const typeList = [
-	'primary', 'info', 'warning', 'danger'
+	'primary', 'info', 'warning', 'danger', 'success'
 ]
+
+const classPrifix = 'vi-button--'
 
 export default {
 	props: {
@@ -27,18 +29,53 @@ export default {
 		icon: {
 			type: String,
 		},
+
+		plain: {
+			type: Boolean,
+			default: false,
+		},
+
+		round: {
+			type: Boolean,
+			default: false,
+		},
+
+		circle: {
+			type: Boolean,
+			default: false
+		}
 	},
 
 	data(){
 		return {
-			className: 'primary'
+			className: classPrifix + 'default'
 		}
 	},
 
 	created(){
+		const classList = []
+
 		if ( typeList.indexOf(this.type) !== -1  ){
-			this.className = this.type
+			classList.push(classPrifix + this.type)
+		}else{
+			classList.push(this.className)
 		}
+
+		if ( this.plain ){
+			classList.push('is-plain')
+		}
+
+		if ( this.round ){
+			classList.push('is-round')
+		} 
+
+		if ( this.circle ){
+			classList.push('is-circle')
+		}
+
+		this.className = classList.join(' ')
+
+		console.log(this.className)
 	},
 
 	methods: {
@@ -53,7 +90,6 @@ export default {
 	display: inline-block
 	line-height: 1
 	white-space: nowrap
-	appearance: none
 	font-size: 14px
 	font-weight: 500
 	text-align: center
@@ -67,11 +103,104 @@ export default {
 	box-sizing: border-box
 	cursor: pointer
 	transition: 0.1s
+	&:focus, &:hover 
+		color: #409eff
+		border-color: #c6e2ff
+		background-color: #ecf5ff
 
-.vi-button:focus,
-.vi-button:hover
+.vi-button--primary
+	color: #fff
+	background-color: #409eff
+	border-color: #409eff
+	&:focus, &:hover
+		color: #fff
+		background-color: #66b1ff
+		border-color: #66b1ff
+
+.vi-button--success
+	color: #fff
+	background-color: #67c23a
+	border-color: #67c23a
+	&:focus, &:hover
+		color: #fff
+		background-color: #85ce61
+		border-color: #85ce61
+
+.vi-button--info
+	color: #fff
+	background-color: #909399
+	border-color: #909399
+	&:focus, &:hover
+		color: #fff
+		background-color: #a6a9ad
+		border-color: #a6a9ad
+
+.vi-button--warning
+	color: #fff
+	background-color: #e6a23c
+	border-color: #e6a23c
+	&:focus, &:hover
+		color: #fff
+		background-color: #ebb563
+		border-color: #ebb563
+
+.vi-button--danger
+	color: #fff
+	background-color: #f56c6c
+	border-color: #f56c6c
+	&:focus, &:hover
+		color: #fff
+		background-color: #f78989
+		border-color: #f78989
+
+.vi-button.is-plain:focus,
+.vi-button.is-plain:hover
+	background-color: #fff
+	border-color: #409eff
 	color: #409eff
-	border-color: #c6e2ff
-	background-color: #ecf5ff
 
+.vi-button--primary.is-plain
+	color: #409eff
+	background: #ecf5ff
+	border-color: #b3d8ff
+	&:focus, &:hover
+		background: #409eff
+		border-color: #409eff
+		color: #fff
+
+.vi-button--success.is-plain
+	color: #67c23a
+	background: #f0f9eb
+	border-color: #c2e7b0
+	&:focus, &:hover
+		background: #67c23a
+		border-color: #67c23a
+		color: #fff
+
+.vi-button--info.is-plain
+	color: #909399
+	background: #f4f4f5
+	border-color: #d3d4d6
+	&:focus, &:hover
+		background: #909399
+		border-color: #909399
+		color: #fff
+
+.vi-button--warning.is-plain
+	color: #e6a23c
+	background: #fdf6ec
+	border-color: #f5dab1
+	&:focus, &:hover
+		background: #e6a23c
+		border-color: #e6a23c
+		color: #fff
+		
+.vi-button--danger.is-plain
+	color: #f56c6c
+	background: #fef0f0
+	border-color: #fbc4c4
+	&:focus, &:hover
+		background: #f56c6c
+		border-color: #f56c6c
+		color: #fff
 </style>
