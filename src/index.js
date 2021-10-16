905c419b6fd43cf715b32d00679eff7e1a0c66ef
wangjigang <wangjigang@xingshentech.com>
2021-10-16 10:04:05 +0800

    Fix bug in the package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import VButton from "./components/VButton.vue"
 
-export default {
+const VueButtonSimple = {
+
  install(Vue) {
   // Let's register our component globally
   // https://vuejs.org/v2/guide/components-registration.html
   Vue.component("v-button", VButton);
+
  }
 }
+
+
+export default VueButtonSimple
